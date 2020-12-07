27df8d22d83a5c87a5bbdbee0ba2412cee2878e7
Martin Szemethy <mszemethy1@sheffield.ac.uk>
2020-12-07 20:52:05 +0100

    User now able to switch to Map activity, when they've specified a Title for a New Visit

--- Week 8 Map Solution/app/src/main/java/myapplication/uk/ac/shef/oak/myapplication/NewVisit.java
+++ Week 8 Map Solution/app/src/main/java/myapplication/uk/ac/shef/oak/myapplication/NewVisit.java
@@ -1,6 +1,12 @@
 package myapplication.uk.ac.shef.oak.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +27,39 @@ public class NewVisit extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_visit);
         setActivity(this);
+
+        final EditText titleEditText = (EditText) findViewById(R.id.title_input);
+        final Button startButton = (Button) findViewById(R.id.startButton);
+        titleEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() > 0) {
+                    startButton.setEnabled(true);
+                } else {
+                    startButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String titleText = titleEditText.getText().toString();
+                if (titleText.length() > 0) {
+                    Intent intent = new Intent(view.getContext(), MapsActivity.class);
+                    view.getContext().startActivity(intent);
+                }
+            }
+        });
     }
 
 }
