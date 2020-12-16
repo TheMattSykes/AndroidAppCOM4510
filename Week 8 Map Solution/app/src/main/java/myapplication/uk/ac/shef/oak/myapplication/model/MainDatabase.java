2267658dbbf5ac3e65b05d9ef4ccfc45bdceb5ef
Owen Hickman <okhickman1@sheffield.ac.uk>
2020-12-16 19:49:15 +0000

    Incrementing database version so that should work

--- Week 8 Map Solution/app/src/main/java/myapplication/uk/ac/shef/oak/myapplication/model/MainDatabase.java
+++ Week 8 Map Solution/app/src/main/java/myapplication/uk/ac/shef/oak/myapplication/model/MainDatabase.java
@@ -22,7 +22,7 @@ import myapplication.uk.ac.shef.oak.myapplication.Visit;
                 VisitData.class,
                 SensorData.class,
                 ImageData.class},
-        version = 9,
+        version = 10,
         exportSchema = false)
 public abstract class MainDatabase extends RoomDatabase {
     public abstract VisitDAO visitDAO();
